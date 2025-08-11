dbcc6650bfd2913dd6a8bed5d1e6cfba928cfb80
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-08-11 05:26:40 +0300

    formatted

--- admin_service/bot/config.py
+++ admin_service/bot/config.py
@@ -1,15 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Set
 from dotenv import load_dotenv
+
 load_dotenv()
 
+
 class Settings(BaseSettings):
-    BOT_TOKEN: str  
-    
+    BOT_TOKEN: str
+
     model_config = SettingsConfigDict(
-        env_file='.env', 
-        env_file_encoding='utf-8',
-        extra='ignore' 
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
-settings = Settings()  
+
+settings = Settings()
--- teams_service/app/services/bot_client.py
+++ teams_service/app/services/bot_client.py
@@ -2,23 +2,20 @@ import httpx
 from fastapi import HTTPException
 from config import settings
 
+
 class BotClient:
     @staticmethod
-    async def send_team_request_to_bot(
-        leader_id: int,
-        team_name: str,
-        org_name: str
-    ):
+    async def send_team_request_to_bot(leader_id: int, team_name: str, org_name: str):
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.post(
-                    "http://admin_bot:8009/team-requests", 
+                    "http://admin_bot:8009/team-requests",
                     json={
                         "leader_id": leader_id,
                         "team_name": team_name,
                         "org_name": org_name,
                     },
-                    timeout=5.0
+                    timeout=5.0,
                 )
                 return response.status_code == 200
         except Exception as e:
