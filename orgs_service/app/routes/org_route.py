5e3fa536dfc73079549261b6194d7be1fed8bc90
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-08-11 05:26:40 +0300

    formatted

--- orgs_service/app/routes/org_route.py
+++ orgs_service/app/routes/org_route.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.session import get_db
@@ -9,14 +8,13 @@ from fastapi import status
 
 router = APIRouter(prefix="/organizations", tags=["Organizations"])
 
+
 @router.get("/exists/{org_name}")
-async def check_organization_exists(
-    org_name: str,
-    db: AsyncSession = Depends(get_db)
-):
+async def check_organization_exists(org_name: str, db: AsyncSession = Depends(get_db)):
     org = await OrgsCRUD.get_org_by_name(db, org_name)
     return {"exists": org is not None}
 
-@router.post('/create/', status_code=status.HTTP_201_CREATED)
+
+@router.post("/create/", status_code=status.HTTP_201_CREATED)
 async def add_org_new(data: OrgCreateSchema, db: AsyncSession = Depends(get_db)):
     return await OrgsCRUD.add_org_by_name(db, data)
