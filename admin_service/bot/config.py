7b9f874576a3c7794327e2525e0893219f442cb8
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-08-11 03:29:00 +0300

    added admin_bot

---
+++ admin_service/bot/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Set
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str  
+    
+    model_config = SettingsConfigDict(
+        env_file='.env', 
+        env_file_encoding='utf-8',
+        extra='ignore' 
+    )
+
+settings = Settings()  
