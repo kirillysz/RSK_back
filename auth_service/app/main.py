8d8b7fc1f47da6f04b7de34591947e6307fb58f3
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-08-20 18:28:09 +0300

    added test cors middleware , will be replaced and protected in future

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes.users_router.router import router as user_router
 from services.rabbitmq import init_rabbitmq
 
 app = FastAPI(title='FastAPI',description='xxx',docs_url='/')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 
--- orgs_service/app/main.py
+++ orgs_service/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from routes.org_route import router as orgs_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="FastAPI", description="xxx", docs_url="/")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(orgs_router)
