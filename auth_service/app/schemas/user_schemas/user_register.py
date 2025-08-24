6bd14110e96f38e3fa30acef22f4678c39fcc119
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-08-24 06:25:19 +0300

    added validation for email

--- auth_service/app/schemas/user_schemas/user_register.py
+++ auth_service/app/schemas/user_schemas/user_register.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,Field,EmailStr
 from pydantic.types import SecretStr
 
 class UserRegister(BaseModel):
     password: SecretStr = Field(..., min_length=8, example="password1232305")
     name: str = Field(..., example="userexample")
-    email: str = Field(...,example="email@email.com")
+    email: EmailStr = Field(...,example="email@email.com")
